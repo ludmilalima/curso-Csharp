866260510a9b88a9795e1bd6fd53bc5466f532bf
Talita <ludmilalima@outlook.com>
2021-04-07 16:47:16 -0300

    exercício produto aula 42

---
+++ exercicios-propostos/aula42/Aula42/Aula42/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula42
+{
+    class Produto
+    {
+        private string Nome;
+        private double Preco;
+        private int Quantidade;
+        public Produto()
+        {
+            Console.WriteLine("Instanciar");
+            Console.WriteLine("Produto:");
+            this.Nome = Console.ReadLine();
+            Console.WriteLine("Preço:");
+            this.Preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade:");
+            this.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Produto: {this.Nome}\nPreço: {this.Preco:F2}\nQuantidade: {this.Quantidade}");
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return (this.Preco * this.Quantidade);
+        }
+        public void AdicionarProdutos(int qtd)
+        {
+            Console.WriteLine("Adicionar");
+            Console.WriteLine($"Quantidade anterior: {this.Quantidade}");
+            this.Quantidade += qtd;
+            Console.WriteLine($"Quantidade atual: {this.Quantidade}");
+        }
+        public void RemoverProdutos(int qtd)
+        {
+            Console.WriteLine("Remover");
+            Console.WriteLine($"Quantidade anterior: {this.Quantidade}");
+            this.Quantidade -= qtd;
+            Console.WriteLine($"Quantidade atual: {this.Quantidade}");
+        }
+    }
+}
---
+++ exercicios-propostos/aula42/Aula42/Aula42/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto();
+            produto1.AdicionarProdutos(4);
+            produto1.AdicionarProdutos(2);
+            produto1.RemoverProdutos(7);
+            Console.WriteLine($"Valor total em estoque: {produto1.ValorTotalEmEstoque()}");
+        }
+    }
+}
